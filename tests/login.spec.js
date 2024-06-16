2a7823aa574b50dc0143a443e7303afa8ef25c95
carolemesc <carolemesc@gmail.com>
2024-06-16 12:13:09 -0300

    teste de login invalido

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -20,7 +20,7 @@ test.describe('Login', () => {
     await page.locator('#mat-input-1').fill(data.USER.SENHA)
     await page.waitForTimeout(1000)
     let btnEntrar = await page.locator('.btn-localiza').isVisible()
-    while (btnEntrar) {
+    while (btnEntrar) { //esse while não é o ideal. existe um bug na ferramenta e mesmo depois do clique no botão "entrar". A intenção aqui é fazer a automação seguir os steps, não resolver um bug 
       await page.locator('.btn-localiza').click()
       await page.waitForTimeout(1000)
       btnEntrar = await page.locator('.btn-localiza').isVisible()
@@ -30,5 +30,20 @@ test.describe('Login', () => {
     await Helper.dismissCookie()
     await Helper.popupJourney()
     await expect(page.getByText(data.USER.NOME)).toBeVisible()
+  })
+
+  test.only('should not be possible to log in with invalid data', async () =>{
+    await page.getByTestId('loc-login').click()
+    await page.locator('#EmailCpf').click()
+    await page.locator('#mat-input-0').fill('email@mail.com')
+    await page.locator('#mat-input-1').fill('password@')
+    await page.waitForTimeout(1000)
+    let errorMessage = await page.locator('#msgsAlerts').isHidden()
+    while (errorMessage) { //esse while não é o ideal. existe um bug na ferramenta e mesmo depois do clique no botão "entrar". A intenção aqui é fazer a automação seguir os steps, não resolver um bug 
+      await page.locator('.btn-localiza').click()
+      await page.waitForTimeout(2000)
+      errorMessage = await page.locator('#msgsAlerts').isHidden()
+    }
+    await expect(page.locator('#msgsAlerts')).toBeVisible()
   })
 })
